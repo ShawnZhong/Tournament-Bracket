c7df5d5a519f41603e88f6fd51fd9b11421517f0
Shawn Zhong <public@shawnzhong.com>
2018-04-17 16:50:11 -0500

    Refactor,

--- src/application/Tournament.java
+++ src/application/Tournament.java
@@ -10,44 +10,43 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Tournament {
     @FXML
     private Pane pane;
 
-    private List<Team> teamList = new ArrayList<>();
+    private int teamSize;
 
-    private Team[][] teamInGame;
-    
+    private Team[][] teams;
+
     @FXML
     private void loadTeamInfo(ActionEvent event) {
         pane.getChildren().clear();
-        teamList.clear();
+        initialize(loadFile());
+        render();
+    }
 
-        List<String> lines = loadFile();
-        int size = lines.size();
-
-//        for (int i = 0; i < size; i++)
-//            teamList.add(new Team(lines.get(i)));
-        
-        teamInGame = new Team[(int)(Math.log(size)/Math.log(2))+1][];
-        for(int i = 0; i < teamInGame.length; i++) {
-        	teamInGame[teamInGame.length - i - 1] = new Team[(int)Math.pow(2, i)];
-        }
-        
-        for(int i = 0; i < teamInGame[0].length; i++) {
-        	if(i%2 == 0) 
-        		teamInGame[0][i] = new Team(lines.get(i));
-        	else
-        		teamInGame[0][teamInGame[0].length-i] = new Team(lines.get(i));
+    private void initialize(List<String> lines) {
+        teamSize = lines.size();
+        teams = new Team[(int) (Math.log(teamSize) / Math.log(2)) + 1][];
+        for (int i = 0; i < teams.length; i++) {
+            teams[teams.length - i - 1] = new Team[(int) Math.pow(2, i)];
         }
 
-        for (int i = 0; i < size; i++) {
-            Team team = new Team(teamInGame[0][i].getName());
-            team.setLayoutX(50 + ((i < size / 2) ? 0 : 750));
-            team.setLayoutY(30 + 60 * (i % (size / 2)));
+        for (int i = 0; i < teams[0].length; i++) {
+            if (i % 2 == 0)
+                teams[0][i] = new Team(lines.get(i));
+            else
+                teams[0][teams[0].length - i] = new Team(lines.get(i));
+        }
+    }
+
+    private void render() {
+        for (int i = 0; i < teamSize; i++) {
+            Team team = new Team(teams[0][i].getName());
+            team.setLayoutX(50 + ((i < teamSize / 2) ? 0 : 750));
+            team.setLayoutY(30 + 60 * (i % (teamSize / 2)));
             team.setOnMouseClicked(e -> {
                 Team t = (Team) e.getSource();
                 TextInputDialog dialog = new TextInputDialog();
@@ -69,6 +68,14 @@ public class Tournament {
             } catch (IOException e) { }
     }
 
+
+    @FXML
+    private void nextRound() {
+
+        // Add code for comparison
+
+        render();
+    }
 
     @FXML
     private void exit(ActionEvent event) {
