219f3b9d3f006837f991cd6e9bc2347354e3dce2
Shawn Zhong <public@shawnzhong.com>
2018-04-20 15:46:31 -0500

    Minor

--- src/application/Tournament.java
+++ src/application/Tournament.java
@@ -17,6 +17,7 @@ package application;
 //2018 Mar 28, 2018  Tournament.java 
 ////////////////////////////80 columns wide //////////////////////////////////
 
+import com.sun.istack.internal.Nullable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -53,10 +54,32 @@ public class Tournament {
      *
      * @param filePath the path of the teamList file.
      */
-    void initialize(String filePath) {
-        loadFile(filePath);
+    void initialize(@Nullable String filePath) {
+        initializeData(filePath);
         initializePane();
         initializeTeam();
+    }
+
+    /**
+     * This method loads file by the path
+     *
+     * @param filepath the path of the file to be read.
+     */
+    private void initializeData(String filepath) {
+        try {
+            lines = Files.readAllLines(Paths.get(filepath));
+        } catch (Exception e) {
+            if (filepath != null) // Used for demo
+                new Alert(Alert.AlertType.WARNING, "File not found. Use demo data instead").showAndWait();
+            ChoiceDialog<Integer> dialog = new ChoiceDialog<>(teamSize, 1, 2, 4, 8, 16);
+            dialog.setTitle("Choose team size");
+            dialog.setHeaderText("Demo mode");
+            dialog.setContentText("Choose your team size:");
+            dialog.showAndWait().ifPresent(integer -> teamSize = integer);
+            lines = IntStream.range(1, teamSize + 1).mapToObj(i -> "Team " + String.format("%02d", i)).collect(Collectors.toList());
+        }
+        teamSize = lines.size();
+        totalRound = 31 - Integer.numberOfLeadingZeros(teamSize);// calculate the total rounds of the competition
     }
 
     private void initializePane() {
@@ -93,6 +116,13 @@ public class Tournament {
 
     /**
      * This method matches which two teams to compete with each other.
+     * See https://oeis.org/A208569
+     * Example:
+     * 1;
+     * 1,  2;
+     * 1,  4, 2, 3;
+     * 1,  8, 4, 5, 2,  7, 3,  6;
+     * 1, 16, 8, 9, 4, 13, 5, 12, 2, 15, 7, 10, 3, 14, 6, 11;
      *
      * @param n
      * @param k
@@ -100,29 +130,6 @@ public class Tournament {
      */
     private int shuffle(int n, int k) {
         return n == 0 ? 1 : k % 2 == 1 ? (1 << n) + 1 - shuffle(n - 1, k / 2) : shuffle(n - 1, k / 2);
-    }
-
-    /**
-     * This method loads file by the path, throws IOException if file not found.
-     *
-     * @param filepath the path of the file to be read.
-     * @return a list of strings that contains the list of the teams.
-     */
-    private void loadFile(String filepath) {
-        try {
-            lines = Files.readAllLines(Paths.get(filepath));
-        } catch (Exception e) {
-            if (filepath != null) // Used for demo
-                new Alert(Alert.AlertType.WARNING, "File not found. Use demo data instead").showAndWait();
-            ChoiceDialog<Integer> dialog = new ChoiceDialog<>(teamSize, 1, 2, 4, 8, 16);
-            dialog.setTitle("Choose team size");
-            dialog.setHeaderText("Demo mode");
-            dialog.setContentText("Choose your team size:");
-            dialog.showAndWait().ifPresent(integer -> teamSize = integer);
-            lines = IntStream.range(1, teamSize + 1).mapToObj(i -> "Team " + String.format("%02d", i)).collect(Collectors.toList());
-        }
-        teamSize = lines.size();
-        totalRound = 31 - Integer.numberOfLeadingZeros(teamSize);// calculate the total rounds of the competition
     }
 
 
@@ -135,46 +142,50 @@ public class Tournament {
             return;
         }
 
-        if (!team.isCompleteRound()) {
-            TextInputDialog dialog = new TextInputDialog();
-            dialog.setTitle("Input Score");
-            dialog.setContentText("Score for " + team.getName() + ": ");
-            while (true) {
-                try {
-                    dialog.showAndWait().ifPresent(s -> {
-                        Double score = Double.valueOf(s);
-                        if (score < 0) throw new InputMismatchException();
-                        team.setScore(score);
-                        compareScore(team);
-                    });
-                    break;
-                } catch (Exception e) {
-                    new Alert(Alert.AlertType.WARNING, "Invalid input. Please try again.").showAndWait();
-                }
+        if (team.isCompleteRound())
+            return;
+
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Input Score");
+        dialog.setContentText("Score for " + team.getName() + ": ");
+        while (true) {
+            try {
+                dialog.showAndWait().ifPresent(s -> {
+                    Double score = Double.valueOf(s);
+                    if (score < 0) throw new InputMismatchException();
+                    team.setScore(score);
+                    compareScore(team);
+                });
+                break;
+            } catch (Exception e) {
+                new Alert(Alert.AlertType.WARNING, "Invalid input. Please try again.").showAndWait();
             }
         }
+
     }
 
     private void compareScore(Team team1) {
         int index = pane.getChildren().indexOf(team1);
         Team team2 = getTeam((index % 2 == 0) ? index - 1 : index + 1);
-        if (team2 != null && team2.getScore() != null) {
-            if (team1.compareTo(team2) == 0) {
-                new Alert(Alert.AlertType.WARNING, team1.getName() + " and " + team2.getName() + " tie!"
-                        + "\r\nStart another game! ").showAndWait();
-                team1.setScore(null);
-                team2.setScore(null);
-                return;
-            }
+        if (team2 == null || team2.getScore() == null)
+            return;
 
-            Team parent = getTeam((index - 1) / 2);
-            parent.setName(team1.compareTo(team2) > 0 ? team1.getName() : team2.getName());
-            parent.setVisible(true);
-            team1.setCompleteRound(true);
-            team2.setCompleteRound(true);
-            if (parent.equals(getTeam(0)))
-                new Alert(Alert.AlertType.INFORMATION, parent.getName() + " wins!!!").showAndWait();
+        if (team1.compareTo(team2) == 0) {
+            new Alert(Alert.AlertType.WARNING, team1.getName() + " and " + team2.getName() + " tie!"
+                    + "\r\nStart another game! ").showAndWait();
+            team1.setScore(null);
+            team2.setScore(null);
+            return;
         }
+
+        Team parent = getTeam((index - 1) / 2);
+        parent.setName(team1.compareTo(team2) > 0 ? team1.getName() : team2.getName());
+        parent.setVisible(true);
+        team1.setCompleteRound(true);
+        team2.setCompleteRound(true);
+        if (parent.equals(getTeam(0)))
+            new Alert(Alert.AlertType.INFORMATION, parent.getName() + " wins!!!").showAndWait();
+
     }
 
     private Team getTeam(int index) { return (Team) pane.getChildren().get(index); }
